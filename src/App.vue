230874035ef5df90a25b8c9307af8a1733feee55
kishan62435 <kishan62435@gmail.com>
2025-08-19 19:44:51 +0530

    feat: add full frontend codebase

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h1>Welcome to Vuetify</h1>
+            <p>This is a sample text</p>
+            <v-btn color="primary">Click Me</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
